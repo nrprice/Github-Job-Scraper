c8f0fadec45e1566a2d7516e6f809f45a97f2060
acrobaticenglishman <63313551+acrobaticenglishman@users.noreply.github.com>
2021-01-18 17:52:53 +0000

    Print removal

--- Scrape.py
+++ Scrape.py
@@ -107,9 +107,6 @@ port = 465
 context = ssl.create_default_context()
 
 
-print (plain)
-
-
-# with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-#     server.login(user, password)
-#     server.sendmail(user, address, message.as_string())
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(user, password)
+    server.sendmail(user, address, message.as_string())
